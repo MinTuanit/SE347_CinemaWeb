37959a10860c5170591c9cde0002d5f95a7ffaea
MinTuan2124 <166462832+MinTuanit@users.noreply.github.com>
2025-11-02 23:15:50 +0700

    Add ticket service

--- src/modules/tickets/tickets.module.ts
+++ src/modules/tickets/tickets.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TicketsService } from './tickets.service';
 import { TicketsController } from './tickets.controller';
+import { SupabaseModule } from 'src/config/supabase.module';
 
 @Module({
+  imports: [SupabaseModule],
   controllers: [TicketsController],
   providers: [TicketsService],
 })
-export class TicketsModule {}
+export class TicketsModule { }
