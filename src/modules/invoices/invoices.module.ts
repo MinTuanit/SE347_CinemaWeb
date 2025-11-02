c426578068335566da6e81607788eb1b2fbd164c
MinTuan2124 <166462832+MinTuanit@users.noreply.github.com>
2025-11-02 23:28:59 +0700

    Add Invoice services

--- src/modules/invoices/invoices.module.ts
+++ src/modules/invoices/invoices.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { InvoicesService } from './invoices.service';
 import { InvoicesController } from './invoices.controller';
+import { SupabaseModule } from 'src/config/supabase.module';
 
 @Module({
+  imports: [SupabaseModule],
   controllers: [InvoicesController],
   providers: [InvoicesService],
 })
-export class InvoicesModule {}
+export class InvoicesModule { }
